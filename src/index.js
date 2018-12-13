34d9e7f4848209490370d46809cd2dc3417693ed
rllola <rllola80@gmail.com>
2018-12-13 04:17:01 +0100

    Actually import updater

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { app, BrowserWindow } from 'electron';
+import updater from './updater';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
