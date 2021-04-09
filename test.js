d342a4e901453bb3379ad242371373fca87d5dee
rllola <rllola80@gmail.com>
2021-04-10 01:18:06 +0200

    end-to-end testing for the releases

---
+++ test.js
@@ -0,0 +1,20 @@
+const test = require('ava');
+const axios = require('axios').default;
+const APP_VERSION = require('./package.json').version
+const FAKE_APP_VERSION = "0.0.1"
+const AUTO_UPDATE_URL = 'https://api.update.rocks/update/github.com/rllola/electron-example/stable/'
+
+test('test debian release', async t => {
+    const response = await axios.get(AUTO_UPDATE_URL + 'linux/' + FAKE_APP_VERSION)
+    t.is(response.data.name, `v${APP_VERSION}`)
+});
+
+test('test macOS release', async t => {
+    const response = await axios.get(AUTO_UPDATE_URL + 'darwin/' + FAKE_APP_VERSION)
+    t.is(response.data.name, `v${APP_VERSION}`)
+});
+
+test('test window release', async t => {
+    const response = await axios.get(AUTO_UPDATE_URL + 'win32/' + FAKE_APP_VERSION)
+    t.is(response.data.name, `v${APP_VERSION}`)
+});
