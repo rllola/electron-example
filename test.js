9677379f7cf70b0ae785d8a193c87028684b50c5
rllola <rllola80@gmail.com>
2021-04-10 14:18:03 +0200

    Check release url

--- test.js
+++ test.js
@@ -6,15 +6,21 @@ const AUTO_UPDATE_URL = 'https://api.update.rocks/update/github.com/rllola/elect
 
 test('test debian release', async t => {
     const response = await axios.get(AUTO_UPDATE_URL + 'linux/' + FAKE_APP_VERSION)
+    console.log(response.data)
     t.is(response.data.name, `v${APP_VERSION}`)
+    t.not(response.data.url, '', 'No release found')
 });
 
 test('test macOS release', async t => {
     const response = await axios.get(AUTO_UPDATE_URL + 'darwin/' + FAKE_APP_VERSION)
+    console.log(response.data)
     t.is(response.data.name, `v${APP_VERSION}`)
+    t.not(response.data.url, '', 'No release found')
 });
 
 test('test window release', async t => {
     const response = await axios.get(AUTO_UPDATE_URL + 'win32/' + FAKE_APP_VERSION)
+    console.log(response.data)
     t.is(response.data.name, `v${APP_VERSION}`)
+    t.not(response.data.url, '', 'No release found')
 });
