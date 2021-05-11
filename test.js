60b2958aa7366bad073e48adb5f44ed6b947bdad
rllola <rllola80@gmail.com>
2021-05-11 17:04:32 +0200

    fix url

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 const APP_VERSION = require('./package.json').version
 const FAKE_APP_VERSION = "0.0.1"
 const AUTO_UPDATE_URL = 'https://api.update.rocks/update/github.com/rllola/electron-example/stable/'
-const AUTO_UPDATE_DEV_URL = 'https://api.update.rocks/update/github.com/rllola/electron-example/stable/'
+const AUTO_UPDATE_DEV_URL = 'https://api.dev.update.rocks/update/github.com/rllola/electron-example/stable/'
 
 test('test debian release', async t => {
     const response = await axios.get(AUTO_UPDATE_URL + 'linux/' + FAKE_APP_VERSION)
