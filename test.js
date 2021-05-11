7508c9f8cd9997a319a235de73ce368a27b4b041
rllola <rllola80@gmail.com>
2021-05-11 13:33:52 +0200

    Adding dev tests

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const axios = require('axios').default;
 const APP_VERSION = require('./package.json').version
 const FAKE_APP_VERSION = "0.0.1"
 const AUTO_UPDATE_URL = 'https://api.update.rocks/update/github.com/rllola/electron-example/stable/'
+const AUTO_UPDATE_DEV_URL = 'https://api.update.rocks/update/github.com/rllola/electron-example/stable/'
 
 test('test debian release', async t => {
     const response = await axios.get(AUTO_UPDATE_URL + 'linux/' + FAKE_APP_VERSION)
@@ -20,6 +21,27 @@ test('test macOS release', async t => {
 
 test('test window release', async t => {
     const response = await axios.get(AUTO_UPDATE_URL + 'win32/' + FAKE_APP_VERSION)
+    console.log(response.data)
+    t.is(response.data.name, `v${APP_VERSION}`)
+    t.not(response.data.url, '', 'No release found')
+});
+
+test('test debian release (DEV)', async t => {
+    const response = await axios.get(AUTO_UPDATE_DEV_URL + 'linux/' + FAKE_APP_VERSION)
+    console.log(response.data)
+    t.is(response.data.name, `v${APP_VERSION}`)
+    t.not(response.data.url, '', 'No release found')
+});
+
+test('test macOS release (DEV)', async t => {
+    const response = await axios.get(AUTO_UPDATE_DEV_URL + 'darwin/' + FAKE_APP_VERSION)
+    console.log(response.data)
+    t.is(response.data.name, `v${APP_VERSION}`)
+    t.not(response.data.url, '', 'No release found')
+});
+
+test('test window release (DEV)', async t => {
+    const response = await axios.get(AUTO_UPDATE_DEV_URL + 'win32/' + FAKE_APP_VERSION)
     console.log(response.data)
     t.is(response.data.name, `v${APP_VERSION}`)
     t.not(response.data.url, '', 'No release found')
