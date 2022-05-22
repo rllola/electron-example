1c5f1fd5664e84d616ca7fbbca57cd8eb4a5b787
rllola <rllola80@gmail.com>
2022-05-22 21:46:24 +0200

    we need the env

--- add-osx-cert.sh
+++ add-osx-cert.sh
@@ -1,15 +1,11 @@
 KEY_CHAIN=build.keychain
 CERTIFICATE_P12=certificate.p12
 
-echo "Adding identity to chain"
-
 # Recreate the certificate from the secure environment variable
 echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12
 
 #create a keychain
 security create-keychain -p github $KEY_CHAIN
-
-echo "Key chain created"
 
 # Make the keychain the default so identities are found
 security default-keychain -s $KEY_CHAIN
