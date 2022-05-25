c1103f62396dfef6d4edd1f395c066e7c3e93be4
sleepy ramen <rllola80@gmail.com>
2022-05-25 19:20:13 +0200

    Migrate to github actions (#36)

--- add-osx-cert.sh
+++ add-osx-cert.sh
@@ -5,19 +5,19 @@ CERTIFICATE_P12=certificate.p12
 echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12
 
 #create a keychain
-security create-keychain -p travis $KEY_CHAIN
+security create-keychain -p github $KEY_CHAIN
 
 # Make the keychain the default so identities are found
 security default-keychain -s $KEY_CHAIN
 
 # Unlock the keychain
-security unlock-keychain -p travis $KEY_CHAIN
+security unlock-keychain -p github $KEY_CHAIN
 
 security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
 
 security find-identity -v
 
-security set-key-partition-list -S apple-tool:,apple: -s -k travis $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k github $KEY_CHAIN
 
 # remove certs
 rm -fr *.p12
