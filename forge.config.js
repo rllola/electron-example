92d108c16b58b291481f39095b741d1131b4b64c
rllola <rllola80@gmail.com>
2023-03-05 16:08:57 +0100

    try using afterPrune; release 0.0.187

--- forge.config.js
+++ forge.config.js
@@ -1,46 +1,58 @@
+const fs = require('fs')
+
 module.exports = {
-  "makers": [
+  hooks: {
+    packageAfterPrune: async (buildPath, electronVersion, platform, arch) => {
+      console.log(`platform : ${platform}`)
+      console.log(`Build path ${buildPath}`)
+      if (platform = 'darwin') {
+        console.log("We are on macOS !")
+        //fs.unlinkSync(path)
+      }
+    }
+  }
+  makers: [
     {
-    "name": '@electron-forge/maker-deb',
-    "config": {
-      "options": {
-        "maintainer": 'Lola Rigaut-Luczak',
-        "homepage": 'https://update.rocks'
+      name: '@electron-forge/maker-deb',
+      config: {
+        options: {
+          maintainer: 'Lola Rigaut-Luczak <lola@update.rocks>',
+          homepage: 'https://update.rocks'
       }
     }
   },
   {
-    "name": '@electron-forge/maker-dmg'
+    name: '@electron-forge/maker-dmg'
   },
   {
-    "name": '@electron-forge/maker-zip',
-    "platforms": [
-      "darwin"
+    name: '@electron-forge/maker-zip',
+    platforms: [
+      'darwin'
     ]
   },
   {
-    "name": '@electron-forge/maker-squirrel',
-    "config": {
-      "name": "electron_example",
-      "certificateFile": "./example-electron.pfx",
-      "certificatePassword": "Amsterdam"
+    name: '@electron-forge/maker-squirrel',
+    config: {
+      name: 'electron_example',
+      certificateFile: './example-electron.pfx',
+      'certificatePassword': 'Amsterdam'
     }
   }],
-  "packagerConfig": {
-    "asar": true,
-    "osxSign": true,
-    "osxNotarize": {
-      "appleId": process.env['APPLE_ID'],
-      "appleIdPassword": process.env['APPLE_ID_PASSWORD'],
-      "ascProvider": process.env['APPLE_TEAM_ID']
+  packagerConfig: {
+    asar: true,
+    osxSign: true,
+    osxNotarize: {
+      appleId: process.env['APPLE_ID'],
+      appleIdPassword: process.env['APPLE_ID_PASSWORD'],
+      ascProvider: process.env['APPLE_TEAM_ID']
     }
   },
-  "publishers" : [{
-    "name": '@electron-forge/publisher-github',
-    "config": {
-      "repository": {
-        "owner": "rllola",
-        "name": "electron-example"
+  publishers : [{
+    name: '@electron-forge/publisher-github',
+    config: {
+      repository: {
+        owner: 'rllola',
+        name: 'electron-example'
       }
     }
   }]
